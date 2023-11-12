96dab813455836c412919b17f60d957e732c7399
Shid <shidtmind@gmail.com>
2023-11-12 09:16:07 +0100

    Extended to Air sensor testing

--- ATmega32U4/PIO_Testing/.pio/libdeps/teensy2/DHT sensor library/examples/DHT_Unified_Sensor/DHT_Unified_Sensor.ino
+++ ATmega32U4/PIO_Testing/.pio/libdeps/teensy2/DHT sensor library/examples/DHT_Unified_Sensor/DHT_Unified_Sensor.ino
@@ -1,31 +1,37 @@
-#include <Arduino.h>
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-/*------------ DEFINITIONS -----------*/
-//Pins
-#define DHTPIN PINB0
-#define DHTPWR PINB1
+#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
 
-//Modules
-#define DHTTYPE    DHT22
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-void setup()
-{
-  // initialize Pins.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(DHTPIN, INPUT);
-  pinMode(DHTPWR, OUTPUT);
-  digitalWrite(DHTPWR, HIGH);
+uint32_t delayMS;
 
-  //initialize serial
-Serial.begin(19200);
+void setup() {
+  Serial.begin(9600);
   // Initialize device.
   dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
-
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -38,7 +44,6 @@ Serial.begin(19200);
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
   Serial.println(F("------------------------------------"));
-
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
   Serial.println(F("Humidity Sensor"));
@@ -49,34 +54,31 @@ Serial.begin(19200);
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
- }
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
 
-void loop()
-{
-  // Flash the led, Sensing cycle must not repeat faster than 0.5Hz
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(10);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1990);
-
-  // Get temperature event and print its value. 
+void loop() {
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
-    Serial.println(F("Error leyendo temperatura!"));
+    Serial.println(F("Error reading temperature!"));
   }
   else {
-    Serial.print(F("Temperatura: "));
+    Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
     Serial.println(F("°C"));
   }
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error leyendo humedad!"));
+    Serial.println(F("Error reading humidity!"));
   }
   else {
-    Serial.print(F("Humedad Relativa: "));
+    Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
